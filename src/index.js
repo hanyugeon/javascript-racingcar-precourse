73a62d490a29276aae67779420e95c234eeb9933
hanyugeon <hanyugeon@naver.com>
2021-11-12 01:43:00 +0900

    refact: 변수명 수정작업

--- src/index.js
+++ src/index.js
@@ -6,43 +6,38 @@ const $countInput = document.getElementById('racing-count-input');
 const $countBtn = document.getElementById('racing-count-submit');
 const $resultForm = document.getElementById('racing-result-form');
 
-function inputName(e) {
+function nameInput(e) {
   e.preventDefault();
-  const nameObj = $nameInput.value.split(',');
+  const carNames = $nameInput.value.split(',');
   
-  nameExecution(nameObj);
+  nameValidator(carNames);
 }
 
-function nameExecution(obj) {
-  const nameAvaliable = obj.every((item) => item.length < 5 && item.length > 0);
+function nameValidator(carNames) {
+  const nameValidate = carNames.every((carName) => carName.length < 5 && carName.length > 0);
 
-  if(nameAvaliable) {
+  if (nameValidate) {
     $nameInput.disabled = true;
     $nameBtn.disabled = true;
     $countForm.style.display = 'block';
-    return;
-  }
-  else {
-    window.alert("이름은 1자 이상 5자 이하만 가능합니다.");
+  } else {
+    alert("이름은 1자 이상 5자 이하만 가능합니다.");
     $nameInput.value = "";
-    return;
   }
 }
 
-function inputCount(e) {
+function countInput(e) {
   e.preventDefault();
   const racingCount = $countInput.value;
-  
-  countExecution(racingCount);
+
+  countValidator(racingCount);
 }
 
-function countExecution(racingCount) {
-  if(racingCount < 1) {
-    window.alert("1이상의 자연수를 입력해 주세요.")
+function countValidator(racingCount) {
+  if (racingCount < 1) {
+    alert("1이상의 자연수를 입력해 주세요.")
     $countInput.value = "";
-    return;
-  }
-  else {
+  } else {
     $countInput.disabled = true;
     $countBtn.disabled = true;
     $resultForm.style.display = 'block';
@@ -52,10 +47,10 @@ function countExecution(racingCount) {
 function init() {
   $countForm.style.display = 'none';
   $resultForm.style.display = 'none';
-  $nameForm.addEventListener('submit', inputName);
-  $nameBtn.addEventListener('click', inputName);
-  $countForm.addEventListener('submit', inputCount);
-  $countBtn.addEventListener('click', inputCount);
+  $nameForm.addEventListener('submit', nameInput);
+  $nameBtn.addEventListener('click', nameInput);
+  $countForm.addEventListener('submit', countInput);
+  $countBtn.addEventListener('click', countInput);
 }
 
 init();
