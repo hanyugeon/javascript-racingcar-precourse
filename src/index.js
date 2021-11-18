40db59636c1bc423c51c500eea0996e2f413a40b
hanyugeon <hanyugeon@naver.com>
2021-11-18 20:07:49 +0900

    feat: 자동차 경주 게임 완성

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import { enableForm, disalbeForm, disableUtils } from "./utils/display-utils.js";
+import { enableForm, disableForm, disableUtil } from "./utils/display-utils.js";
 import { nameValidate, countValidate, clearInput } from "./utils/input-utils.js";
+import { roundScoreUtil, roundResultUtil, raceWinner } from "./utils/race-utils.js";
+import { raceResultPaint, raceWinnerPaint } from "./utils/paint-utils.js";
+
+export const $resultForm = document.getElementById('racing-result-form');
 
 const $nameForm = document.getElementById('car-names-form');
 const $nameInput = document.getElementById('car-names-input');
@@ -7,14 +11,14 @@ const $nameBtn = document.getElementById('car-names-submit');
 const $countForm = document.getElementById('racing-count-form');
 const $countInput = document.getElementById('racing-count-input');
 const $countBtn = document.getElementById('racing-count-submit');
-const $resultForm = document.getElementById('racing-result-form');
 
 function nameInput(e) {
   e.preventDefault();
   const carNames = $nameInput.value.split(',');
   
   if (nameValidate(carNames)) {
-    disableUtils($nameInput, $nameBtn);
+    disableUtil($nameInput);
+    disableUtil($nameBtn);
     enableForm($countForm);
   } else {
     clearInput($nameInput);
@@ -26,7 +30,8 @@ function countInput(e) {
   const racingCount = $countInput.value;
 
   if (countValidate(racingCount)) {
-    disableUtils($countInput, $countBtn);
+    disableUtil($countInput);
+    disableUtil($countBtn);
     enableForm($resultForm);
     raceStart();
   } else {
@@ -36,70 +41,23 @@ function countInput(e) {
 
 function raceStart() {
   const carNames = $nameInput.value.split(',');
-  const racingCount = $countInput.value
-  let resultArr = new Array(carNames.length).fill(0);
-  let winnerArr = [];
+  const racingCount = $countInput.value;
+  let roundScore = new Array(carNames.length).fill(0);
+  let raceResult = '';
 
   for (let i = 0; i < racingCount; i++) {
-    raceUtil(resultArr);
-    console.log(resultArr);
+    roundScoreUtil(roundScore);
+    raceResult += roundResultUtil(carNames, roundScore);
   }
-
-  raceWinner(resultArr, winnerArr);
-  // racePaint();
-  resultPaint(winnerArr);
-}
-
-function raceUtil(arr) {
-  let raceResult = 0;
   
-  for (let i = 0; i < arr.length; i++) {
-    raceResult = Math.floor(Math.random() * 10)
-    console.log(raceResult);
-    if (raceResult > 4) {
-      arr[i] += 1;
-    }
-  }
+  raceResultPaint(raceResult);
+  raceWinner(carNames, roundScore);
+  raceWinnerPaint();
 }
-
-function raceWinner(resultArr, winnerArr) {
-  const winnerPoint = Math.max.apply(null, resultArr);
-  
-  for (let i = 0; i < resultArr.length; i++) {
-    if(resultArr[i] === winnerPoint) {
-      winnerArr.push(i);
-    }
-  }
-  console.log(winnerPoint)
-  console.log(winnerArr);
-}
-
-function racePaint(winner) {
-  const $raceCar = document.createElement('li');
-  $raceCar.setAttribute("id", "racing-car");
-  $raceCar.innerHTML = `
-    
-  `
-
-  
-}
-
-function resultPaint(winnerArr) {
-  const carNames = $nameInput.value.split(',');
-  let winner = ''
-
-  for (let i = 0; i < winnerArr.length; i++) {
-    winner += carNames[winnerArr[i]]
-  }
-
-  $resultForm.innerHTML = `<span id="racing-winners"> 최종 우승자: ${winner} </span>`
-}
-
-
 
 function init() {
-  disalbeForm($countForm);
-  disalbeForm($resultForm);
+  disableForm($countForm);
+  disableForm($resultForm);
   $nameForm.addEventListener('submit', nameInput);
   $nameBtn.addEventListener('click', nameInput);
   $countForm.addEventListener('submit', countInput);
